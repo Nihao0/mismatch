d4411b79b249fd64352c89eed4557fa2fca50707
Nihao0 <45843275+Nihao0@users.noreply.github.com>
2019-04-29 16:32:02 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,22 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <?php
 require_once('connectvars.php');
 
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
-	//Имя пользователя или его пароль не были введены,
-	//поэтому отправляются заголовки аутентификации
-	header('HTTP/1.1 401 Unauthorized');
-	header('WWW-Authenticate: Basic realm="Mismatch"');
-	exit('<h3>Mismatch</h3>Sorry,you must enter your password and username.');
-}
+//Обнуление сообщения об ошибке
+$error_msg = "";
+//Если пользователь не вошел в приложение,попытка войти
+if (!isset($_COOKIE['user_id'])) {
+	
+	if(isset($_POST['submit'])) {
+
 
 //Соединение с базой данных
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 //Получение введенных пользователем данных для аутентификации
-$user_username = mysqli_real_escape_string($dbc, trim($_SERVER['PHP_AUTH_USER']));
-$user_password = mysqli_real_escape_string($dbc, trim($_SERVER['PHP_AUTH_PW']));
-
+$user_username = mysqli_real_escape_string($dbc, trim($_POST['username']));
+$user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
+if (!empty($user_username) && !empty($user_password)) {
 //Поиск имени пользователя и его пароля в базе данных 
 $query = "SELECT user_id, username FROM mismatch_user WHERE username = '$user_username' AND password = SHA('$user_password')";
 $data = mysqli_query($dbc, $query);
@@ -24,10 +25,55 @@ if (mysqli_num_rows($data) == 1){
 	//Процедура входа прошла нормально,присваиваем переменным значения
 	//идентификатора пользователя и его пароля 
 	$row = mysqli_fetch_array($data);
-	$user_id = $row['user_id'];
-	$username = $row['username'];
+	setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30)); //срок действия 30 дней
+	setcookie('username', $row['username'],  time() + (60 * 60 * 24 * 30)); //срок действия 30 дней
+	$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
+	header('Location: ' . $home_url);
 }
-
-//Подтверждение успешного входа в приложение 
-echo('<p class="login">You logged as ' . $username . '.</p>');
+else {
+	//Имя пользователя или его пароль введены не верно создание сообщения об ошибке
+	$error_msg = 'Sorry, you must enter your username and password/';
+	}
+}
+else {	
+	$error_msg = 'Sorry, you must enter your username and password/';
+	}
+  }
+}	
 ?>
+<html>
+	<head>
+		<title>Mismatch. Log in.</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<h3>Mismatch. Log in.</h3>
+
+<?php
+//Если куки не содержит данныйх,выводятся сообщение об ошибке
+//и форма входа в приложение, в противном случае подтверждение входа
+if (empty($_COOKIE['user_id'])) {
+	echo '<p class="error"' . $error_msg . '</p>';
+?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<fieldset>
+		<legend>Log in</legend>
+		<label for="username">Username:</label>
+		<input type="text" name="username"
+			value="<?php if (!empty($user_username)) echo $user_username; ?>" /><br />
+		<label for="password">Password:</label>
+		<input type="password" name="password" />
+	</fieldset>
+	<input type="submit" value="Log in" name="submit" />
+</form>
+
+<?php
+}
+else {
+	//подтверждение успешного входа в приложение
+	echo('<p class="login">You logged as ' . $_COOKIE['username'] . '.</p>');
+}
+?>
+</body>
+<html>
